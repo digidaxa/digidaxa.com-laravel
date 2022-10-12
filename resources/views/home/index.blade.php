794b9777429f9765e633e5763d6313aea9dba1a2
virgiawankusuma <virgiawan150@gmail.com>
2022-10-12 15:48:07 +0700

    change single product link

--- resources/views/home/index.blade.php
+++ resources/views/home/index.blade.php
@@ -8,15 +8,15 @@
     <div class="flex flex-wrap px-4 mt-2 gap-x-5 gap-y-6 justify-start">
       <!-- looping for 5 time -->
       @foreach ($products as $product)
-        <a href={{ '/products' .$product->slug }} class="w-[calc(50%-.8rem)] rounded-lg border shadow-sm overflow-hidden md:w-[calc((100%/3)-1rem)] lg:w-[calc(25%-1rem)]">
+        <a href={{ '/products/' .$product->slug }} class="w-[calc(50%-.8rem)] rounded-lg border shadow-sm overflow-hidden md:w-[calc((100%/3)-1rem)] lg:w-[calc(25%-1rem)]">
           <img 
             src={{ "/img/product/" . $product->thumbnail }}
             class="object-cover aspect-[4/3] w-full"
           >
           <div class="relative">
             <div class="m-2 text-left">
-              <h3 class="text-sm font-semibold">{{ $product->name }}</h3>
-              <h4 class="text-xs">{{ $product->category->name }}</h4>
+              <h3 class="text-sm font-semibold text-dark">{{ $product->name }}</h3>
+              <h4 class="text-xs text-secondary">{{ $product->category->name }}</h4>
             </div>
             {{-- love icon --}}
             <div class=" absolute top-1/2 right-5">
